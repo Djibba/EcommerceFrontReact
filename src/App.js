28de9acee49ed5d73aceb20e27fec592023b9a20
Djibba <djibba16084@gmail.com>
2023-06-08 12:37:20 +0000

    add files Js components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import FloatingCart from './Components/FloatingCart/FloatingCart';
+import Contact from './Pages/Contact/Contact';
+import ShoppingCart from './Pages/ShopppingCart/ShoppingCart';
+import Products from './Pages/Products/Products';
+import ProductShowcase from './Pages/ProductShowcase/ProductShowcase';
 
 function App() {
   return (
     <>
+      <Navbar />
+      <FloatingCart />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/produits' element={<Products />} />
+        <Route path='/produits/:id' element={<ProductShowcase />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/shoppingcart' element={<ShoppingCart />} />
+      </Routes>
 
     </>
   );
